52f84f90ae724cf8acc4e4c3033d61c47b77969e
dwaned <dwane.debono@gmail.com>
2020-01-21 11:56:48 +0100

    Initial commit:  - Setting the environment to env-fixer  - Setting urls and api_key for Fixer  - Creating the custom steps  - Creating the tests feature file  - Removed examples folder  - Updated Allure download url in pom file

---
+++ src/test/java/io/dtective/quality/framework/CustomSteps.java
@@ -0,0 +1,58 @@
+package io.dtective.quality.framework;
+
+import com.google.gson.Gson;
+import cucumber.api.java.en.When;
+import io.dtective.quality.framework.data.DataTestCore;
+import io.dtective.test.TestDataCore;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class CustomSteps {
+    @When("I use response and save random {int} currencies in CSV file {string}")
+    public void iUseResponseAndSaveRandomCurrenciesInCSVFile(int numCurrencies, String file) throws IOException {
+
+        /*
+            Getting the rates from the latest HTTP response.
+            'resbody' is a special data-store that the framework uses to store the body of last performed HTTP call.
+            JSONPath is used to retrieve the rates.
+            The value of $.rates is then saved to another data-store named rates.
+        */
+
+        DataTestCore dataTestCore = new DataTestCore();
+        dataTestCore.iGetValueOfKeyFromJSONInLocalParamAndStoreItInLocalParam("$.rates", "resbody", "rates");
+
+        /*
+            Converting the value for the key 'rates' containing JSON into a Map using Gson library.
+            Then converting the Map to an Object[] to be able to select any index from the Array.
+        */
+
+        Map ratesMap = new Gson().fromJson(TestDataCore.getDataStore("rates").toString(), Map.class);
+        Object[] ratesObj = ratesMap.entrySet().toArray();
+
+       /*
+        Getting x random key value pairs from the rates and storing them in CSV
+        */
+
+        Map<String, String> output = new HashMap<String, String>();
+
+        Random rnd = new Random();
+        int pos = 0;
+
+        for (int i = 0; i <= numCurrencies; i++) {
+            pos = rnd.nextInt(ratesObj.length);
+            Map.Entry value = (Map.Entry) ratesObj[pos];
+            output.put(value.getKey().toString(), value.getValue().toString());
+        }
+
+        FileWriter csvWriter = new FileWriter(file);
+
+        for (Map.Entry<String, String> rowData : output.entrySet()) {
+            csvWriter.append(rowData.getKey() + "," + rowData.getValue() + "\n");
+        }
+        csvWriter.close();
+
+    }
+}
---
+++ src/test/resources/quality/fixer/ApiTests.feature
@@ -0,0 +1,22 @@
+@fixer
+Feature: Testing Fixer API
+  
+  # Setting the API_KEY as a data-store available to any scenario below.
+  Background:
+    Given I add the following data-store "API_KEY" "89e502dd1619ca38ebf9eca1d79ae3c7"
+  
+
+  Scenario:  Get All currencies and store 10 in CSV
+
+    # Checking that site is responsive.
+    Given I send a GET to URL "{Site}"
+    And response code is "200"
+
+    # Getting the latest currency rates.
+    When I send a GET to URL "{Api}/latest?access_key={API_KEY}"
+
+    # Getting 10 random currencies from the above response and storing them into a CSV.
+    And I use response and save random 10 currencies in CSV file "currencies.csv"
+
+    # Asserting that the CSV file has been created.
+    Then the file "currencies.csv" exists
